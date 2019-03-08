9fc180221416bd40330dfb64a13141c6666f908a
RuiLinZhu <824772535@qq.com>
2019-03-08 12:26:59 +0800

    commit hello

---
+++ AoP/src/main/java/com/spring/Aop/Hello1.java
@@ -0,0 +1,11 @@
+package com.spring.Aop;
+
+/**
+ * Created by User on 2019/3/8.
+ */
+public class Hello1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
